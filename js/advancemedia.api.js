2ca0ebe36985193a8f06690318b2e9f8ba7becb9
Nelson Suarez <nelsonsuare@gmail.com>
2013-09-16 19:55:12 -0430

    Integracion de la funcion getTuned
    Se integro la funcion getTuned para obeter info del canal sintonizado

--- js/advancemedia.api.js
+++ js/advancemedia.api.js
@@ -1,82 +1,56 @@
 $(document).ready(function() {
 
-	function sendTuned(channel){
-		/*$.ajax({
-			cache    : true,
-			dataType : 'jsonp',
-			url      : "http://10.1.42.213:8080/tv/tune?major="+channel,
-			type     : 'GET',
-			success  : function(data, textStatus, jqXHR) {
-				console.log(JSON.stringify(data) + " / " + textStatus + " / " + JSON.stringify(jqXHR));
-			}
-		});	
+	var IP_Directv = "192.168.1.147"; // Colocar aqui la IP de tu DVR.
+	
+	function getTuned(){
+		// Info de Canal
 		$.ajax({
 			cache    : true,
 			dataType : 'jsonp',
-			url      : "http://10.1.42.206:8080/tv/tune?major="+channel,
+			url      : "http://"+ IP_Directv +":8080/tv/getTuned", 
 			type     : 'GET',
 			success  : function(data, textStatus, jqXHR) {
 				console.log(JSON.stringify(data) + " / " + textStatus + " / " + JSON.stringify(jqXHR));
-			}
-		});	*/
-		$.ajax({
-			cache    : true,
-			dataType : 'jsonp',
-			url      : "http://192.168.1.147:8080/tv/tune?major="+channel,
-			type     : 'GET',
-			success  : function(data, textStatus, jqXHR) {
-				console.log(JSON.stringify(data) + " / " + textStatus + " / " + JSON.stringify(jqXHR));
-				alert("Msg: "+data.status.msg+" / Canal: "+channel);
-			},
-			error	 : function(data, textStatus, jqXHR) {
-				console.log(JSON.stringify(data) + " / " + textStatus + " / " + JSON.stringify(jqXHR));
-				alert("Msg: "+data.status.msg+" / Canal: "+channel);
+				$( "#reload" ).hide( "fast" );
+				$("#getTuned").html('<span class="help-block"><strong>Canal: </strong> ' + data.callsign + '</span> <span class="help-block"><strong>Num Canal: </strong> ' + data.major + '</span> <span class="help-block"><strong>Titulo: </strong> ' + data.title + '</span> <span class="help-block"><strong>Episodio: </strong> ' + data.episodeTitle + '</span> <span class="help-block"><strong>Rating: </strong> ' + data.rating + '</span> <span class="help-block"><strong>Duracion: </strong> ' + data.duration + ' Seg</span>');
+				
 			}
 		});
-		
-		/*var cadena = $.getJSON("http://192.168.1.147:8080/tv/tune?major="+channel, function (data) {
-			console.log(data.status.msg);
-			/*for (l in data.libros.lib) {
-			   cad += ("<br />" + data.libros.lib[l].libro + " de " +
-				   data.libros.lib[l].autores[0] + ", " +
-				   data.libros.lib[l].autores[1] + " y " +
-				   data.libros.lib[l].autores[2] + "<br />Editorial: " +
-				   data.libros.lib[l].editorial +
-				   "<br />").replace("y undefined", "").replace(", undefined", "");
-			}
-			$("#texto").html(cad);
-		});*/
 	}
 	
+	function sendTuned(channel){
+		// Cambio de Canal
+		$.ajax({
+			cache    : true,
+			dataType : 'jsonp',
+			url      : "http://"+ IP_Directv +":8080/tv/tune?major="+channel,
+			type     : 'GET',
+			success  : function(data, textStatus, jqXHR) {
+				console.log(JSON.stringify(data) + " / " + textStatus + " / " + JSON.stringify(jqXHR));
+				$("#getTuned").html('<div><img src="img/loader.gif"/></div>');
+				$( "#reload" ).show( "fast" );
+				setTimeout(function() {
+					getTuned();
+				}, 1000);
+			}
+		});
+	}
+	
+	// Validación (Se puede mejorar mucho)
 	function validChannel(channel){
 		if (channel >=100 && channel < 2000) return true;
 		else return false;
 	}
 	
 	$( "#sendTuned" ).click(function() {
-	
 		if (validChannel($("#tuned").val())) sendTuned($( "#tuned" ).val());
 		else alert("Ese Canal no existe");
 	});
 	
-	$( "#espn" ).click(function() {
-		sendTuned("620");
+	$( "#reloadTuned" ).click(function() {
+		// Recargo la página
+		location.reload();
 	});
 	
-	$( "#espn2" ).click(function() {
-	  sendTuned("624");
-	});
-	$( "#espn3" ).click(function() {
-	  sendTuned("626");
-	});
-	$( "#fox" ).click(function() {
-	  sendTuned("606");
-	});
-	$( "#fox2" ).click(function() {
-	  sendTuned("608");
-	});
-	$( "#fox3" ).click(function() {
-	  sendTuned("609");
-	});
-	
+	getTuned();
 });
